190588b8711179c2b6ff8eee9a09d90364ad0e78
Miguel Etzel García Delgado <miguel.garcia@mccollect.mx>
2025-07-15 09:47:03 -0600

    Funcionalidad botones cargar y regresar

--- Calculadora-de-eficiencia/Form1.cs
+++ Calculadora-de-eficiencia/Form1.cs
@@ -6,5 +6,35 @@ namespace Calculadora_de_eficiencia
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel_archivo.Visible = false;
+            panel_archivo.SendToBack();
+            panel_inicio.Visible = true;
+            panel_inicio.BringToFront();
+
+
+
+
+        }
+
+        private void boton_cargar_Click(object sender, EventArgs e)
+        {
+            panel_archivo.Visible = true;
+            panel_archivo.BringToFront();
+            panel_inicio.Visible = false;
+            panel_inicio.SendToBack();
+        }
+
+        
+
+        private void boton_regresar_Click_1(object sender, EventArgs e)
+        {
+            panel_archivo.Visible = false;
+            panel_archivo.SendToBack();
+            panel_inicio.Visible = true;
+            panel_inicio.BringToFront();
+        }
     }
 }
